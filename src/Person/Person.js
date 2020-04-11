f9ece53143e83d47a062d9d3a6d48e5d7d2c6d00
Saddam-tech <salokhiddinov0727@gmail.com>
2020-04-11 15:20:17 +0900

    ssss

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,7 +4,7 @@ const person = (props) => {
   return (
     <div>
       <p>
-        I am a {props.name} and I am {props.age} years old!
+        I am {props.name} and I am {props.age} years old!
       </p>
       <p>{props.children}</p>
     </div>
