36efa64305c6f468d37e8d8967318a15df6425a9
Saddam-tech <salokhiddinov0727@gmail.com>
2020-05-05 13:07:03 +0900

    styled-components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
-import Radium, { StyleRoot } from "radium";
 
 class App extends Component {
   state = {
@@ -102,19 +101,17 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
-        <div className="App">
-          <h1>Hey Buddies</h1>
-          <p className={classes.join(" ")}>This is really working!</p>
-          <button style={style} onClick={this.togglePersonsHandler}>
-            Toggle persons
-          </button>
+      <div className="App">
+        <h1>Hey Buddies</h1>
+        <p className={classes.join(" ")}>This is really working!</p>
+        <button style={style} onClick={this.togglePersonsHandler}>
+          Toggle persons
+        </button>
 
-          {persons}
-        </div>
-      </StyleRoot>
+        {persons}
+      </div>
     );
   }
 }
 
-export default Radium(App);
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,26 @@
 import React from "react";
-import "./Person.css";
-import Radium from "radium";
+// import "./Person.css";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+      width: 60%;
+      margin: 16px auto;
+      border: 1px solid #ccc;
+      box-shadow: 0 2px 3px gray;
+      padding: 16px;
+      text-align: center;
+ 
+       background-color: white;
+       border: 1px solid blue ;
+       font: inherit;
+       padding: 8px;
+       cursor: pointer;
+ 
+  
+    "@media (min-width: 500px)": {
+      width: "450px",
+    },
+`;
 
 const person = (props) => {
   const style = {
@@ -21,4 +41,4 @@ const person = (props) => {
   );
 };
 
-export default Radium(person);
+export default person;
