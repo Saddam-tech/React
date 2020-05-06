3c958b8cab1ee34660a580a87928c10618a4c6c7
Saddam-tech <salokhiddinov0727@gmail.com>
2020-05-06 16:40:52 +0900

    I like css modules more

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,32 +1,18 @@
 import React from "react";
-// import "./Person.css";
-import styled from "styled-components";
-
-const StyledDiv = styled.div`
-      width: 60%;
-      margin: 16px auto;
-      border: 1px solid #ccc;
-      box-shadow: 0 2px 3px gray;
-      padding: 16px;
-      text-align: center;
-  
-    "@media (min-width: 500px)": {
-      width: "450px",
-    },
-`;
+import classes from "./Person.css";
+// import styled from "styled-components";
 
 const person = (props) => {
   return (
     // <div className="Person" style={style}>
-    <StyledDiv>
+    <div className={classes.Person}>
       <p onClick={props.click}>
         I am {props.name} and I am {props.age} years old! I like playing{" "}
         {props.hobby}!
       </p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
-    </StyledDiv>
-  
+    </div>
   );
 };
 
